7cc8022a9f148897e6b075418f6ad4f8c09a612d
heliguy4599 <95997226+heliguy4599@users.noreply.github.com>
2024-02-26 22:04:00 -0500

    Create flake.nix

---
+++ flake.nix
@@ -0,0 +1,21 @@
+# /etc/nixos/flake.nix
+{
+  description = "flake for GA402R";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixos-unstable";
+    };
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      GA402R = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
